5b24e81465d4ff3da455723f69882477602c882f
Live session user <albrechter@msoe.edu>
2018-07-16 22:36:25 -0500

    Adds 4-8

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,4 +1,4 @@
-choice = input("Choose 1-3\n")
+choice = input("Choose 1-8\n")
 choice = int(choice)
 if choice == 1:
     print("First is the worst")
@@ -6,5 +6,15 @@ elif choice == 2:
     print("Second is the best")
 elif choice == 3:
     print("Third is the one with the treasure chest")
+elif choice == 4:
+    print("Fourth goes up north")
+elif choice == 5:
+    print("Fifth gets a gift")
+elif choice == 6:
+    print("Six picks up Styx")
+elif choice == 7:
+    print("Seven goes to heaven")
+elif choice == 8:
+    print("This nine now... deal with it")
 else:
     print("Y'all can't count???")
