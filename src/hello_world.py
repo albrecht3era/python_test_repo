bea5b26dfca235fd580e1de58dc091b13c9f84d7
Live session user <albrechter@msoe.edu>
2018-07-16 22:17:30 -0500

    This adds initial number choice

---
+++ src/hello_world.py
@@ -0,0 +1,10 @@
+choice = input("Choose 1-3\n")
+choice = int(choice)
+if choice == 1:
+    print("First is the worst")
+elif choice == 2:
+    print("Second is the best")
+elif choice == 3:
+    print("Third is the one with the treasure chest")
+else:
+    print("Y'all can't count???")
